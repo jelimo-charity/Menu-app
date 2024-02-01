f7c05ae6833d498c8cbe99f7393021c3e7d07812
jelimo-charity <charityjelimo893@gmail.com>
2024-02-01 21:27:08 +0300

    mapped data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-
-import './App.css'
+import React from 'react';
+import './App.css';
+import menu from './components/data.js'; // Replace 'path-to-your-data' with the actual path to your data.js file
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <section>
+      <div className="title">
+        <h1>Our Menu</h1>
+        <div className="underline"></div>
+      </div>
+      <div className="data">
+        {menu.map((item) => (
+          <div key={item.id} className="menu-item">
+            <img src={item.img} alt={item.title} />
+            <div className="item-info">
+              <header>
+                <h4>{item.title}</h4>
+                <p className="price">${item.price}</p>
+              </header>
+              <p className="item-text">{item.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
